c0508ddd0f0e592ae5c3d26bd6101c5734d22963
Archita Rai <68114193+archita784@users.noreply.github.com>
2021-05-29 11:11:34 +0530

    Add files via upload

---
+++ spring-mvc-bookapp-jdbc/src/main/java/com/bookapp/model/Book.java
@@ -0,0 +1,66 @@
+package com.bookapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	private String title;
+	private Integer bookId;
+	private String author;
+	private String category;
+	private Double price;
+	
+	
+	
+	public Book() {
+		super();
+	}
+	public Book(String title, Integer bookId, String author, String category, Double price) {
+		super();
+		this.title = title;
+		this.bookId = bookId;
+		this.author = author;
+		this.category = category;
+		this.price = price;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+}
