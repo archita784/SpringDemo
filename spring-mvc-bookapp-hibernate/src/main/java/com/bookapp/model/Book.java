eb8dff0acf537fe664f1e52f82047f9ecea9f31c
Archita Rai <68114193+archita784@users.noreply.github.com>
2021-05-29 11:10:37 +0530

    Add files via upload

---
+++ spring-mvc-bookapp-hibernate/src/main/java/com/bookapp/model/Book.java
@@ -0,0 +1,61 @@
+package com.bookapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Book {
+	private String title;
+	@Id
+	private Integer bookId;
+	private String author;
+	private String category;
+	private Double price;
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", bookId=" + bookId + ", author=" + author + ", category=" + category
+				+ ", price=" + price + "]";
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+}
