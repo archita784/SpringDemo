8e4f52f11a86d3c53978998e3ba45cbfd0a27223
Archita Rai <68114193+archita784@users.noreply.github.com>
2021-05-29 11:12:35 +0530

    Add files via upload

---
+++ spring-mvc-empl/src/main/java/com/emplapp/model/Employee.java
@@ -0,0 +1,37 @@
+package com.emplapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+	String name,city;
+	Integer empId ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Integer getEmpId() {
+		return empId;
+	}
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+	
+	
+}
